8861bf67cb18960a74ca7802a6a1c3a223f478f4
alaqsaka <aqsaabidin15@gmail.com>
2022-01-15 23:37:11 +0700

    footer component progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Jumbotron from "./components/jumbotron";
+import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
 import jumboData from "./fixtures/jumbo.json";
 
 export default function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
---
+++ src/containers/footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Footer } from "../components";
+
+export function FooterContainer() {
+  return (
+    <Footer>
+      <Footer.Title>Questions? Contact Us.</Footer.Title>
+      <Footer.Break />
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="#">Ways to Watch</Footer.Link>
+          <Footer.Link href="#">Coporate Information</Footer.Link>
+          <Footer.Link href="#">Netflix Originals</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Help Centre</Footer.Link>
+          <Footer.Link href="#">Jobs</Footer.Link>
+          <Footer.Link href="#">Terms of Use</Footer.Link>
+          <Footer.Link href="#">Contact Us</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Account8</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
+    </Footer>
+  );
+}
