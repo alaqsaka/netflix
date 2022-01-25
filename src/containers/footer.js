56ae2c6437896add2c126fff8fe9afafbeb15aa6
alaqsaka <aqsaabidin15@gmail.com>
2022-01-25 19:13:29 +0700

    footer done

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -23,9 +23,21 @@ export function FooterContainer() {
         </Footer.Column>
 
         <Footer.Column>
-          <Footer.Link href="#">Account8</Footer.Link>
+          <Footer.Link href="#">Account</Footer.Link>
+          <Footer.Link href="#">Reedem Gift Cards</Footer.Link>
+          <Footer.Link href="#">Privacy</Footer.Link>
+          <Footer.Link href="#">Speed Test</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Media Centre</Footer.Link>
+          <Footer.Link href="#">Buy Gift Cards</Footer.Link>
+          <Footer.Link href="#">Cookie Preferences</Footer.Link>
+          <Footer.Link href="#">Legal Notices</Footer.Link>
         </Footer.Column>
       </Footer.Row>
+      <Footer.Break />
+      <Footer.Text>Netflix Indonesia</Footer.Text>
     </Footer>
   );
 }
