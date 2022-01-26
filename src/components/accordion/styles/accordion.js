0e1b75a943314bd2754968029a9e0a68e2462546
alaqsaka <aqsaabidin15@gmail.com>
2022-01-27 06:21:11 +0700

    feat: accordion components done

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -13,10 +13,6 @@ export const Inner = styled.div`
   margin: auto;
 `;
 
-export const Frame = styled.div`
-  margin-bottom: 40px;
-`;
-
 export const Title = styled.h1`
   font-size: 50px;
   line-height: 1.1;
@@ -33,6 +29,7 @@ export const Title = styled.h1`
 export const Item = styled.div`
   color: white;
   margin-bottom: 10px;
+  max-width: 670px;
 
   &:first-of-type {
     margin-top: 3em;
@@ -65,4 +62,19 @@ export const Header = styled.div`
   }
 `;
 
-export const Body = styled.div``;
+export const Body = styled.div`
+  max-height: 1200px;
+  transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+  background: #303030;
+  padding: 0.8em 2.2em 0.8em 1.2em;
+  white-space: pre-wrap;
+  user-select: none;
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+    line-height: 22px;
+  }
+`;
