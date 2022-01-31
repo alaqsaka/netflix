5b770f92027d9a5da4b10117429d95a3766806a9
alaqsaka <aqsaabidin15@gmail.com>
2022-01-31 14:41:36 +0700

    create new component callade feature, for title and subtitle

---
+++ src/components/feature/styles/feature.js
@@ -0,0 +1,15 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div`
+  text-align: center;
+  padding: 10px;
+`;
+
+export const Title = styled.h1`
+  color: white;
+`;
+
+export const SubTitle = styled.h3`
+  color: white;
+  text-transform: uppercase;
+`;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { OptForm, Feature } from "../components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
@@ -16,10 +17,28 @@ export default function Home() {
           element={
             <>
               <HeaderContainer>
-                <JumbotronContainer />
-                <FaqsContainer />
-                <FooterContainer />
+                <Feature>
+                  <Feature.Title>
+                    Unlimited films, TV programmes and more.
+                  </Feature.Title>
+
+                  <Feature.SubTitle>
+                    Watch Anywhere. Cancel At Any Time.
+                  </Feature.SubTitle>
+                </Feature>
+                <OptForm>
+                  <OptForm.Input placeholder="Email Address" />
+                  <OptForm.Button>Try It Now</OptForm.Button>
+                  <OptForm.Break />
+                  <OptForm.Text>
+                    Ready to watch? Enter your email to create or restart your
+                    membership
+                  </OptForm.Text>
+                </OptForm>
               </HeaderContainer>
+              <JumbotronContainer />
+              <FaqsContainer />
+              <FooterContainer />
             </>
           }
         ></Route>
